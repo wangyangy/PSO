7f9cb1eb986af58bb08265c736574a16dfbda0f0
wangyang <1176574597@qq.com>
2018-10-29 15:43:49 +0800

    "完成了多次运行"

--- src/PSOVersion1/Main.java
+++ src/PSOVersion1/Main.java
@@ -2,17 +2,24 @@ package PSOVersion1;
 import java.io.IOException;
 
 
+
 public class Main 
 {
 	public static void main(String[] args) throws IOException 
 	{
-		for(int i=0;i<30;i++) {
+		long start = System.currentTimeMillis();
+		for(int i=0;i<10;i++) {
 			PSO pso = new PSO();
 			pso.readfile();
 			pso.createpso();
 			pso.Initialize();
-			pso.Search();			
+			pso.Search();
+			//每次跑完一遍这个静态数组都要重新赋值的
+			Agent.gbest = new double[Agent.iAgentDim*Agent.ikmeans]; 
+			Agent.result.clear();
 		}
+		long end = System.currentTimeMillis();
+		System.out.println("运行时间："+(end-start)/1000+"秒");
 	}
 }
 
